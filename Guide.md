6c4028e7036fd1e359edf2e02479f77abf2f853c
Samee Ullah <samee.ullah@bitflex.com>
2022-07-16 17:41:34 +0500

    - useDebounce without useFetch hook implementation with local data array.

---
+++ Guide.md
@@ -0,0 +1,10 @@
+#Introduction
+In this tutorial we are going to perform live
+search from the SWAPI api with the help of
+the useDebounce hook to limit the number
+of requests
+
+## Before we get started
+Make sure to check out the branch for
+useFetchHook. we will be using it in this
+snippet.
--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import {useEffect, useState} from "react";
+import useDebounce from "./hooks/use-debounce";
+
+const DATA = ['bike', 'dog', 'coin', 'cat', 'shop', 'turtle'];
 
 function App() {
+  const [result, setResults] = useState([]);
+  const [text, setText] = useState("");
+
+  const deb = useDebounce(text, 500);
+
+  useEffect(() => {
+    const d = DATA.filter(el => el.toLowerCase().includes(deb));
+    setResults(d);
+  }, [deb]);
+
   return (
     <div className="App">
-      {/* Todo */}
+      <input
+        type="text"
+        value={text}
+        onChange={e => setText(e.target.value)}
+      />
+      {
+        result.length > 0 ?
+          result.map((el, i) => <div key={i}>{el}</div>) : <div>No result</div>
+      }
     </div>
   );
 }
